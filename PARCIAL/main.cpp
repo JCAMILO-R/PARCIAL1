6893923e6e25c2962470b397d862ece2f0414690
JCamilo <juan.restrepo55@udea.edu.co>
2020-10-17 15:55:27 -0500

    inicializando las variables y pensando en cuales funciones iniciales voy a necesitar y de que tipo seran.

---
+++ PARCIAL/main.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+
+using namespace std;
+float alturao=0, alturad=0, distancia=0,velocidadO=0,angulo=0,radioO=0, radioD=0;
+//otra forma de  las variables es realizando un arreglo tipo float de de 7 indices. arregloFloar[6]+
+
+
+float velocidadD();
+
+
+int main()
+{
+   cout<<"ingrese los parametros de iniciales"<<endl;
+   cout<<"Altura del cañon ofensivo"<<endl;
+   cin>> alturao;
+   cout<<"Altura del cañon defensivo"<<endl;
+   cin>> alturad;
+   cout<<"la distancia entre los cañones"<<endl;
+   cin>> distancia;
+   cout<<"Velocidad inicial del proyectil ofensivo"<<endl;
+   cin>>velocidadO;
+   cout<<"angulo inicial del proyectil ofensivo"<<endl;
+   cin>>angulo;
+
+
+
+
+
+
+
+}
